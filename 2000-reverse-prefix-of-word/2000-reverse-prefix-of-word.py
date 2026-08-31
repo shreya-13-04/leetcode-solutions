a817b68de91b06c59661ab4c1834bcc4d019ebb1
Shreya B <shreyabaala2004@gmail.com>
2026-09-01 02:10:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.49%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        
+        idx=word.index(ch)
+
+        result=word[:idx+1][::-1]+word[idx+1:]
+        return result
+
+        
